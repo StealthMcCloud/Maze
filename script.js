c836651bfb3a89f58e617bf0c91206c1c7a42b4e
Clinton Johnson <dragoon1246224@gmail.com>
2018-08-28 10:15:14 -0400

    New changes

---
+++ script.js
@@ -0,0 +1,21 @@
+//Create a keyboard listener to ensure that the piece will move starting from the Start
+//Ensure that the piece can not move through walls
+//
+
+const map = [
+    "WWWWWWWWWWWWWWWWWWWWW",
+    "W   W     W     W W W",
+    "W W W WWW WWWWW W W W",
+    "W W W   W     W W   W",
+    "W WWWWWWW W WWW W W W",
+    "W         W     W W W",
+    "W WWW WWWWW WWWWW W W",
+    "W W   W   W W     W W",
+    "W WWWWW W W W WWW W F",
+    "S     W W W W W W WWW",
+    "WWWWW W W W W W W W W",
+    "W     W W W   W W W W",
+    "W WWWWWWW WWWWW W W W",
+    "W       W       W   W",
+    "WWWWWWWWWWWWWWWWWWWWW"
+];
